9335643e7c1536da39a7c576239639b12da23e63
icju <isicju@gmail.com>
2024-01-07 02:23:29 +0000

    kek

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.io.BufferedInputStream;
+
+public class Main {
+
+    private static final String constant = "keke";
+
+    public static void main(String[] args) {
+
+        int value = 19/0;
+
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(null);
+        System.out.println(bufferedInputStream.toString());
+
+
+
+
+
+    }
+
+}
